51353533d2b44cf5a32479eb88d58fcf1a9f376d
ThanhPhuongg <phuong306204@gmail.com>
2024-10-04 21:59:38 +0700

    admin-fe

---
+++ app/(admin)/dashboard/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Dashboard() {
+      return (
+        <div>
+          <a>Admin</a>
+        </div>
+      )
+}
--- app/services/productService.js
+++ app/services/productService.js
@@ -1,23 +1,18 @@
 import {ApiService} from "./apiService";
 
-const SessionService = {
+const ProductService = {
     list: async (payload) => {
         ApiService.setHeader();
         let $params = new URLSearchParams(payload).toString();
         let res = await ApiService.get('/api/v1/sessions?' + $params);
         return res.data;
     },
-    create: async (payload) => {
+    detail: async (id) => {
         ApiService.setHeader();
-        let res = await ApiService.post('/api/v1/session', payload);
+        let $params = new URLSearchParams(id).toString();
+        let res = await ApiService.get('/api/v1/sessions?' + $params);
         return res.data;
     },
-    join: async (payload) => {
-        ApiService.setHeader();
-        let res = await ApiService.post('/api/v1/session/join', payload);
-        return res.data;
-    },
-
 
 };
 export default SessionService;
